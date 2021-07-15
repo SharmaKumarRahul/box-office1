bb28a4c89b2ef531f9f465489d88884116343b0d
SharmaKumarRahul <rahulkumarsharma182000@gmail.com>
2021-07-15 21:28:54 +0530

    Created input and fetch result from tvmaze api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MiainPageLayout from '../components/MiainPageLayout';
 
-function Home() {
-  return <MiainPageLayout>This is Home</MiainPageLayout>;
-}
+const Home = () => {
+  const [input, setInput] = useState('');
+
+  const onSearch = () => {
+    // https://api.tvmaze.com/search/shows?q=men
+
+    fetch(`https://api.tvmaze.com/search/shows?q=${input}`)
+      .then(r => r.json())
+      .then(result => {
+        console.log(result);
+      });
+  };
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
+  return (
+    <MiainPageLayout>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
+        Search
+      </button>
+    </MiainPageLayout>
+  );
+};
 
 export default Home;
