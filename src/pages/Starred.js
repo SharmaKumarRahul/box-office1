0a2ea81ca9858c264be501f6cbeaca484c92a8b7
SharmaKumarRahul <rahulkumarsharma182000@gmail.com>
2021-07-21 21:32:11 +0530

    Finished starred page

--- src/pages/Starred.js
+++ src/pages/Starred.js
@@ -1,8 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import MiainPageLayout from '../components/MiainPageLayout';
+import { useShows } from '../misc/custom-hooks';
+import { apiGet } from '../misc/config';
+import ShowGrid from '../components/show/ShowGrid';
 
 function Starred() {
-  return <MiainPageLayout>This is starred.</MiainPageLayout>;
+  const [starred] = useShows();
+
+  const [shows, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (starred && starred.length > 0) {
+      const promise = starred.map(showId => apiGet(`/shows/${showId}`));
+      Promise.all(promise)
+        .then(apiData => apiData.map(show => ({ show })))
+        .then(results => {
+          setShow(results);
+          setIsLoading(false);
+        })
+        .catch(err => {
+          setError(err.message);
+          isLoading(false);
+        });
+    } else {
+      setIsLoading(false);
+    }
+  }, [starred]);
+
+  return (
+    <MiainPageLayout>
+      {isLoading && <div>Shows are still loading</div>}
+      {error && <div>erroe occured: {error}</div>}
+      {!isLoading && !shows && <div>No shows were added</div>}
+      {!isLoading && !error && shows && <ShowGrid data={shows} />}
+    </MiainPageLayout>
+  );
 }
 
 export default Starred;
