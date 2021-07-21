2f07719ae91f2600377724e22160d1fd4b77e794
SharmaKumarRahul <rahulkumarsharma182000@gmail.com>
2021-07-21 23:44:37 +0530

    Updates

---
+++ precache-manifest.6f7a4cc075938a15183c7393359e2e63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "924e7472af21e5a1423cc7548601e954",
+    "url": "/box-office1/index.html"
+  },
+  {
+    "revision": "e12ff1af7f809f036c6c",
+    "url": "/box-office1/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "9bab2a6bd43d33553564",
+    "url": "/box-office1/static/js/2.52d2c6fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/box-office1/static/js/2.52d2c6fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e12ff1af7f809f036c6c",
+    "url": "/box-office1/static/js/main.06b8ecb3.chunk.js"
+  },
+  {
+    "revision": "c4922a5f00893a7c3e11",
+    "url": "/box-office1/static/js/runtime-main.93c18862.js"
+  }
+]);
